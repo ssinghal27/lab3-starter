7fd6bc4d52a8aa0cf61c6c6595dbb8288550007b
Singhal <s2singhal@ieng6-201.ucsd.edu>
2024-10-15 11:50:51 -0700

    yes

--- average.c
+++ average.c
@@ -16,12 +16,13 @@ int main(int argc, char *argv[]) {
 
     // Loop through each command-line argument to calculate the sum
     // First arg starts at argv[1]
-    for (int i = 1; i < argc; i++) {
+    int i;
+    for (i = 1; i < argc; i++) {
         sum += atoi(argv[i]);
     }
 
     // Calculate the average of all args
-    float average = sum / 10;
+    float average = sum / (argc-1);
 
     printf("Average: %.2f\n", average);
 
